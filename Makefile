d4ec98ae4b434520c1f2ee97d96ec5d25c39fd71
Marten Cassel <marten.cassel@gmail.com>
2024-08-31 16:29:01 +0200

    more

--- Makefile
+++ Makefile
@@ -1,3 +1,18 @@
-.PHONY: test-shell
-test-shell:
+SHELL := /bin/bash
+
+help:
+	@grep -E '^[0-9a-zA-Z_-]+:.*?## .*$$|(^#--)' $(MAKEFILE_LIST) \
+	| awk 'BEGIN {FS = ":.*?## "}; {printf "\033[32m %-43s\033[0m %s\n", $$1, $$2}' \
+	| sed -e 's/\[32m #-- /[33m/'
+
+#-- Ubuntu
+ubuntu-2004: ## Run Ubuntu 20.04
+	docker run -it -v $(PWD):/host ubuntu:20.04
+
+
+ubuntu-2204: ## Run Ubuntu 22.04
 	docker run -it -v $(PWD):/host ubuntu:22.04
+
+ubuntu-2404: ## Run Ubuntu 24.04
+	docker run -it -v $(PWD):/host ubuntu:24.04
+
--- install-from-source.sh
+++ install-from-source.sh
@@ -1,7 +1,21 @@
 #!/bin/bash
 set -x
 
+export FOREMAN_PROXY_VERSION="3.12.0-rc1"
+
+# Define variables for different groups of dependencies
 export DEBIAN_FRONTEND=noninteractive
+
+COMMON_TOOLS="curl git sudo wget jq net-tools"
+BUILD_DEV="build-essential libssl-dev pkg-config libvirt-dev libyaml-dev"
+RUBY_DEPENDENCIES="ruby-dev ruby-libvirt bundler"
+SYSTEMD_DEPENDENCIES="libsystemd-dev"
+KERBEROS_DEPENDENCIES="libkrb5-dev krb5-user"
+LDAP_DEPENDENCIES="libldap-dev libsasl2-dev"
+
+FOREMAN_DEPENDENCIES="libvirt-dev libyaml-dev"
+
+
 
 log_file="/tmp/install_from_source.log"
 
@@ -21,45 +35,35 @@ log_error() {
 }
 
 
+update_system() {
+  log_info "Updating system..."
+  apt-get update > >(tee -a $log_file) 2>&1
+  apt-get upgrade -y > >(tee -a $log_file) 2>&1
+}
 
-# Define variables for different groups of dependencies
-COMMON_DEPENDENCIES="curl git sudo wget jq net-tools"
-BUILD_DEPENDENCIES="build-essential libssl-dev pkg-config libvirt-dev libyaml-dev"
-RUBY_DEPENDENCIES="ruby-dev ruby-libvirt bundler"
-SYSTEMD_DEPENDENCIES="libsystemd-dev"
-KERBEROS_DEPENDENCIES="libkrb5-dev krb5-user"
-LDAP_DEPENDENCIES="libldap-dev libsasl2-dev"
 
-# Function to install dependencies
-install_dependencies() {
+instapp_packages() {
+  PKG_LIST=$1
+  # Check if empty
+  if [ -z "$PKG_LIST" ]; then
+    log_info "No packages to install."
+    return
+  fi
   log_info "Installing dependencies with apt-get..."
-  apt-get update && apt-get install -y \
-    $COMMON_DEPENDENCIES \
-    $BUILD_DEPENDENCIES \
-    $RUBY_DEPENDENCIES \
-    $SYSTEMD_DEPENDENCIES \
-    $KERBEROS_DEPENDENCIES \
-    $LDAP_DEPENDENCIES
+  apt-get install -y $PKG_LIST  > >(tee -a $log_file) 2>&1
 }
  
 # Function to clone repositories
 clone_repositories() {
   log_info "Cloning smart-proxy and smart_proxy_realm_ad_plugin repositories..."
   cd ~
+  if [ -d "smart-proxy" ]; then
+    rm -rf smart-proxy
+  fi
   git clone https://github.com/theforeman/smart-proxy.git
-  git clone https://github.com/theforeman/smart_proxy_realm_ad_plugin.git
+ 
 }
-
-# Function to build and install the smart_proxy_realm_ad_plugin
-install_smart_proxy_realm_ad_plugin() {
-  log_info "Building and installing smart_proxy_realm_ad_plugin..."
-  cd ~/smart_proxy_realm_ad_plugin
-  bundle install > >(tee -a $log_file) 2>&1
-  gem build smart_proxy_realm_ad_plugin.gemspec > >(tee -a $log_file) 2>&1
-  gem install smart_proxy_realm_ad_plugin*.gem > >(tee -a $log_file) 2>&1
-  echo "Ending install_smart_proxy_realm_ad_plugin"
-}
-
+ 
 
 # Function to configure smart-proxy
 configure_smart_proxy() {
@@ -87,6 +91,7 @@ configure_smart_proxy() {
   bundle install || { log_info "Bundle install failed"; exit 1; }  > >(tee -a $log_file) 2>&1
 
   # Add the plugin to the Gemfile
+  PLUGIN_DIR=~/smart_proxy_realm_ad_plugin
   echo "gem 'smart_proxy_realm_ad_plugin', :path => '$PLUGIN_DIR'" >> ./bundler.d/Gemfile.local.rb
 
   # Create realm.yml configuration
@@ -170,25 +175,130 @@ check_server_response() {
   return 1
 }
 
-log_info "Installation and configuration script started..."
+export DEBIAN_FRONTEND=noninteractive
+ 
+function install_realmad {
+  log_info "Building and installing smart_proxy_realm_ad_plugin..."
+  cd ~
+  if [ -d "smart_proxy_realm_ad_plugin" ]; then
+    rm -rf smart_proxy_realm_ad_plugin
+  fi
+  git clone https://github.com/theforeman/smart_proxy_realm_ad_plugin.git
+  cd ~/smart_proxy_realm_ad_plugin
+  apt-get -y install bundler 
+  # radcli dependencies
+  apt-get -y install libkrb5-dev libldap-dev libsasl2-dev
+  bundle install > >(tee -a $log_file) 2>&1
+  gem build smart_proxy_realm_ad_plugin.gemspec > >(tee -a $log_file) 2>&1
+  gem install smart_proxy_realm_ad_plugin*.gem > >(tee -a $log_file) 2>&1
+  echo "Ending install_smart_proxy_realm_ad_plugin"
+}
 
-# Main script execution
-install_dependencies
-# Check that ruby is installed
-if ! command -v ruby &> /dev/null
-then
-  log_info "Ruby is not installed. Installing Ruby..."
-  apt-get install -y ruby-full  > >(tee -a $log_file) 2>&1
-fi
-clone_repositories
-install_smart_proxy_realm_ad_plugin
-configure_smart_proxy "EXAMPLE.COM" "realm-proxy@EXAMPLE.COM" "realm_ad.keytab" "dc.example.com" "my_required_for_now_nice_prefix"
-start_smart_proxy
+function install_smartproxy {
+  log_info "Building and installing smart-proxy..."
+  cd ~
+  if [ -d "smart-proxy" ]; then
+    rm -rf smart-proxy
+  fi
+  git clone https://github.com/theforeman/smart-proxy.git
+  cd ~/smart-proxy
+  apt-get -y install libsystemd-dev libyaml-dev libkrb5-dev libvirt-dev
+  bundle install 
+  
+  # Add the plugin to the Gemfile (smart-proxy-realm-ad-plugin)
+  PLUGIN_DIR=~/smart_proxy_realm_ad_plugin
+  echo "gem 'smart_proxy_realm_ad_plugin', :path => '$PLUGIN_DIR'" >> ./bundler.d/Gemfile.local.rb
 
-check_server_response "http://localhost:8000/features"
+  # Configuration dir
+  CONFIG_DIR=~/smart-proxy/config/settings.d
 
-verify_plugins
-manage_host
-check_log_messages
+  # Enable the plugin
+cat>~/smart-proxy/config/settings.d/realm.yml<<EOF
+---
+:enabled: true
+:use_provider: realm_ad
+EOF  
 
-log_info "Installation and configuration complete."
+# Add realm_ad.yml configuration
+
+touch ~/smart-proxy/realm_ad.keytab
+
+# Create realm_ad.yml configuration
+cat > $CONFIG_DIR/realm_ad.yml<<EOF
+---
+:realm: realm_ad.keytab
+:principal: EXAMPLE.COM
+:keytab_path: realm_ad.keytab
+:domain_controller: dc.example.com
+:computername_prefix: "my_prefix"
+EOF
+
+cat ~/smart-proxy/config/settings.d/realm.yml
+cat ~/smart-proxy/config/settings.d/realm_ad.yml
+
+}
+
+function main {
+  log_info "Installation and configuration script started..."
+  update_system
+  apt-get -y install git
+  install_realmad
+  install_smartproxy
+}
+
+
+
+
+
+# # Install dependencies
+# instapp_packages "$COMMON_TOOLS $BUILD_DEV $RUBY_DEPENDENCIES $SYSTEMD_DEPENDENCIES $KERBEROS_DEPENDENCIES $LDAP_DEPENDENCIES $FOREMAN_DEPENDENCIES"
+
+# # # Check that ruby is installed
+# if ! command -v ruby &> /dev/null
+# then
+#   log_info "Ruby is not installed. Installing Ruby..."
+#   apt-get install -y ruby-full  > >(tee -a $log_file) 2>&1
+# fi
+# clone_repositories
+# install_smart_proxy_realm_ad_plugin
+# configure_smart_proxy "EXAMPLE.COM" "realm-proxy@EXAMPLE.COM" "realm_ad.keytab" "dc.example.com" "my_required_for_now_nice_prefix"
+# start_smart_proxy
+
+# check_server_response "http://localhost:8000/features"
+
+# verify_plugins
+# manage_host
+# check_log_messages
+
+# log_info "Installation and configuration complete."
+
+#   cd ~/smart-proxy
+#   apt-get -y install libsystemd-dev libyaml-dev libkrb5-dev libvirt-dev
+#   bundle install
+#   PLUGIN_DIR=~/smart_proxy_realm_ad_plugin
+#   echo "gem 'smart_proxy_realm_ad_plugin', :path => '$PLUGIN_DIR'" >> ./bundler.d/Gemfile.local.rb
+
+#   KEYTAB_FILENAME="realm_ad.keytab"
+#   SMART_PROXY_DIR=~/smart-proxy
+#   PLUGIN_DIR=~/smart_proxy_realm_ad_plugin
+#   CONFIG_DIR=$SMART_PROXY_DIR/config/settings.d
+# cat > $CONFIG_DIR/realm.yml <<EOF
+# ---
+# :enabled: true
+# :use_provider: realm_ad
+# EOF
+#   RELM_NAME="EXAMPLE.COM"
+#   PRINCIPAL="realm-proxy@EXAMPLE.COM"
+#   DOMAIN_CONTROLLER="dc.example.com"
+#   COMPUTERNAME_PREFIX="my_required_for_now_nice_prefix"
+#   touch $FOREMAN_PROXY_DIR/$KEYTAB_FILENAME
+# # Create realm_ad.yml configuration
+# cat > $CONFIG_DIR/realm_ad.yml <<EOF
+# ---
+# :realm: $realm
+# :principal: $principal
+# :keytab_path: $FOREMAN_PROXY_DIR/$keytab_filename
+# :domain_controller: $domain_controller
+# :computername_prefix: $computername_prefix
+# EOF
+
